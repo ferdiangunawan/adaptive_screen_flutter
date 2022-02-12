d5868e300176ec6dfeb4933c1713f0076a1ac67e
ferdiangunawan <ferdiangunawan98@gmail.com>
2022-02-13 02:58:54 +0700

    [WIP] add unit test and widget test

--- test/adaptive_screen_flutter_test.dart
+++ test/adaptive_screen_flutter_test.dart
@@ -1,5 +1,69 @@
+import 'package:adaptive_screen_flutter/adaptive_screen_flutter.dart';
+import 'package:adaptive_screen_flutter/screen_init.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('adds one to input values', () {});
+  group('Adaptive screen test', () {
+    test('init layout', () {
+      var mobile = ScreenInit.tabBreakPoint;
+      expect(mobile, 600.0);
+
+      ScreenInit.setTabBreakPoint(800.0);
+
+      expect(ScreenInit.tabBreakPoint, 800.0);
+    });
+    testWidgets('Mobile layout widget test', (WidgetTester tester) async {
+      await tester.pumpWidget(const MaterialApp(
+        home: AdaptiveScreen(
+          key: Key('AdaptiveWidget'),
+          mobile: Text('ini mobile'),
+        ),
+      ));
+      final textMobile = find.text('ini mobile');
+      final adaptiveWidget = find.byKey(const Key('AdaptiveWidget'));
+
+      expect(textMobile, findsOneWidget);
+      expect(adaptiveWidget, findsWidgets);
+    });
+    testWidgets('Tab layout widget test', (WidgetTester tester) async {
+      await tester.pumpWidget(MaterialApp(
+        home: AdaptiveScreen(
+          key: const Key('AdaptiveWidget'),
+          tab: Container(
+            key: const Key('WidgetTab'),
+            height: 50,
+            width: 50,
+            color: Colors.white,
+            child: const Text('T'),
+          ),
+        ),
+      ));
+      final textTab = find.byKey(const Key('WidgetTab'));
+      final adaptiveWidget = find.byKey(const Key('AdaptiveWidget'));
+
+      expect(textTab, findsOneWidget);
+      expect(adaptiveWidget, findsWidgets);
+    });
+
+    testWidgets('Web layout widget test', (WidgetTester tester) async {
+      await tester.pumpWidget(MaterialApp(
+        home: AdaptiveScreen(
+          key: const Key('AdaptiveWidget'),
+          web: Container(
+            key: const Key('WidgetWeb'),
+            height: 50,
+            width: 50,
+            color: Colors.white,
+            child: const Text('T'),
+          ),
+        ),
+      ));
+      final textWeb = find.byKey(const Key('WidgetWeb'));
+      final adaptiveWidget = find.byKey(const Key('AdaptiveWidget'));
+
+      expect(textWeb, findsOneWidget);
+      expect(adaptiveWidget, findsWidgets);
+    });
+  });
 }
